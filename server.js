06d375daf4d233e39ab7a6e3195e45cf083d4c5d
Ashraf Kamel <a.kamel@karma-systems.com>
2021-09-19 18:08:30 +0200

    CSS ADDED

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
-
 // Require Express to run server and routes
 const express = require('express');
 // Start up an instance of app
@@ -11,14 +10,11 @@ const bodyParser = require('body-parser');
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 // Cors for cross origin allowance
 const cors = require('cors');
 app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
-
-
 // Setup Server
 const port = 3000;
 const server = app.listen(port, listening);
@@ -40,6 +36,6 @@ app.post('/add', postData);
 // POST an Weather Data
 function postData(req,res){
   projectData = req.body;
-  console.log(projectData);
+  //console.log(projectData);
   res.send(projectData);
 };
--- website/app.js
+++ website/app.js
@@ -1,50 +1,63 @@
 /* Global Variables */
+let feeling = '';
+let totalData = {};
 // Personal API Key for OpenWeatherMap API
 const baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
 const units = 'units=metric';
 const apiKey = '9a426edfe26021172fda77da10e1542e';
-let tempreture = 0;
-let pressure = 0;
-let humidity = 0;
-let feeling = "";
-let city = 'Unkown';
-/*
-api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={API key}
-*/
+
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.toDateString();
 // Event listener to add function to existing HTML DOM element
+const errorMessage = document.getElementById('errorMessage');
+
 document.getElementById('generate').addEventListener('click', performAction); 
 /* Function called by event listener */
 function performAction(e){
     const zipCode =  document.getElementById('zip').value;
+    const countryCode =  document.getElementById('country').value;
     feeling =  document.getElementById('feelings').value;
-    getWeatherData(baseURL,zipCode, apiKey).then(updateUI());
+    getWeatherData(zipCode, countryCode).then((data)=>{
+      if(data){
+        /*Collect both user and API Data in one object*/
+        totalData = {
+          temp: Math.round(data.main.temp),
+          pressure: data.main.pressure,
+          city:data.name,
+          hum: data.main.humidity,
+          feeling: feeling,
+          date:newDate
+        };
+      }
+      //console.log(totalData);
+    postData('/add', totalData); //Post data to server
+    updateUI(); // Update user interface
+    });
     
     }   
 /* Function to GET Web API Data*/
-const getWeatherData = async (baseURL, zip, key)=>{
+const getWeatherData = async (zip, country)=>{
     
-    const res = await fetch(`${baseURL}${zip},us&appid=${key}&${units}`)
-    console.log(res);
+    const res = await fetch(`${baseURL}${zip},${country}&appid=${apiKey}&${units}`)
     try {
-  
       const data = await res.json();
-      console.log(data)
-      tempreture = data.main.temp + ' C';
-      pressure = data.main.pressure + ' mbar';
-      humidity = data.main.humidity + ' %';
-      city = data.name;
+      if(data.cod != 200){ //Data successfully fetched 
+        errorMessage.innerHTML = data.message;
+        setTimeout(() => {
+          errorMessage.innerHTML = ''
+        }, 3000);
+      }
+      //console.log(data)
       return data;
     }  catch(error) {
       console.log("error", error);
       // appropriately handle the error
+      alert(`Something went wrong, Error: ${error}!`);
     }
   }
-/* Function to POST data */
+/* Function to POST data to server*/
 const postData = async ( url = '', data = {})=>{
-
   const response = await fetch(url, {
   method: 'POST', 
   credentials: 'same-origin', 
@@ -55,36 +68,29 @@ const postData = async ( url = '', data = {})=>{
 });
 
   try {
-    const newData = await response.json();
-    return newData
+    const dataRecord = await response.json();
+    console.log('Data have been saved to server...')
+    console.log(dataRecord);
+    return dataRecord
   }catch(error) {
   console.log("error", error);
+  alert(`Something went wrong, Error: ${error}!`);
   }
 }
-
-/* Function to GET Project Data */
-const retrieveData = async (url='') =>{ 
-  const request = await fetch(url);
-  try {
-  // Transform into JSON
-  const allData = await request.json()
-  }
-  catch(error) {
-    console.log("error", error);
-    // appropriately handle the error
-  }
-}
-
+/*Fuction to update user interface according to server fetched Data*/
 const updateUI = async () => {
   const request = await fetch('/all');
   try{
-    const allData = await request.json();
-    document.getElementById('city').innerHTML = city;//allData[0].city;
-    document.getElementById('date').innerHTML = newDate;//allData[0].date;
-    document.getElementById('temp').innerHTML = tempreture;//allData[0].temp;
-    document.getElementById('content').innerHTML = feeling;//allData[0].feeling;
+    const dataRecord = await request.json();
+    document.getElementById('date').innerHTML = `Date: ${dataRecord.date}`;
+    document.getElementById('city').innerHTML = dataRecord.city;
+    document.getElementById('temp').innerHTML = `Temperature: ${dataRecord.temp}&deg C`;
+    document.getElementById('pressure').innerHTML = `Pressure: ${dataRecord.pressure} mbar`;
+    document.getElementById('Humidity').innerHTML = `Humidity: ${dataRecord.hum}`;
+    document.getElementById('content').innerHTML = `Feeling: ${dataRecord.feeling}`;
 
   }catch(error){
     console.log("error", error);
+    alert(`Something went wrong, Error: ${error}!`);
   }
 }
